27859a1614815c87e41093f6a8993fe37b2bc554
Khan Saheruddin <khansaheruddin06@gmail.com>
2025-09-15 18:18:58 +0530

    Update Login.jsx

--- taskManagerFrontend/src/components/Login.jsx
+++ taskManagerFrontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
   const [rememberMe, setRememberMe] = useState(false)
 
   const navigate = useNavigate()
-  const url = 'http://localhost:4000'
+  const url = 'https://task-manager-qvi8.onrender.com'
 
   useEffect(() => {
     const token = localStorage.getItem('tokem')
